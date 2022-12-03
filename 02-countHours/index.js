b230483c1bb36a95de41187874296af9f0ca2840
inmacc96 <inmacaballerocarrero@gmail.com>
2022-12-03 14:30:49 +0100

    Refactor code solutions 02

--- 02-countHours/index.js
+++ 02-countHours/index.js
@@ -1,10 +1,6 @@
 function countHours(year, holidays) {
   return holidays.reduce((hoursExtra, holiday) => {
-    const day = holiday.split("/")[1];
     const date = new Date(`${year}/${holiday}`);
-    if (day !== "31" && ![0, 6].includes(date.getDay())) {
-      hoursExtra += 2;
-    }
-    return hoursExtra;
+    return ![0, 6].includes(date.getDay()) ? hoursExtra + 2 : hoursExtra;
   }, 0);
 }
