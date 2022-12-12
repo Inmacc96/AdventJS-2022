14a70662af772a66a7da677e8aea3877c92430c0
inmacc96 <inmacaballerocarrero@gmail.com>
2022-12-12 12:17:48 +0100

    Solve the problem of day 7

---
+++ 07-getGiftsToRefill/index.js
@@ -0,0 +1,5 @@
+function getGiftsToRefill(a1, a2, a3) {
+  return [...new Set(a1), ...new Set(a2), ...new Set(a3)].filter(
+    (gift, _, array) => array.indexOf(gift) === array.lastIndexOf(gift)
+  );
+}
